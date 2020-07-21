f74bcccd257b26662719a22f9f620ba349843b99
TelevisionNinja <TelevisionNinja@gmail.com>
2020-07-21 00:17:59 -0500

    added example & optimized some lines

--- multi_base_numbers.py
+++ multi_base_numbers.py
@@ -4,13 +4,13 @@ def _pad_left(a_list, padding_element, length):
 
 
 def _remove_leading_element(a_list):
-    if a_list:
-        while a_list[0] == 0:
-            del a_list[0]
+    while a_list[0] == 0:
+        del a_list[0]
 
-            if not a_list:
-                a_list = [0]
-                break
+        if not a_list:
+            a_list = [0]
+            break
+    
     return a_list
 
 
@@ -34,7 +34,7 @@ def _compare_numbers_represented_as_arrays(list_1, list_2):
     return 0
 
 
-def _make_lists_equal_length(list_1, list_2, base_bool):
+def _make_lists_equal_length(list_1, list_2):
     length_1 = len(list_1)
     length_2 = len(list_2)
 
@@ -42,11 +42,7 @@ def _make_lists_equal_length(list_1, list_2, base_bool):
         max_len = max(length_1, length_2)
 
         _pad_left(list_1, 0, max_len)
-
-        if base_bool:
-            _pad_left(list_2, list_2[0], max_len)
-        else:
-            _pad_left(list_2, 0, max_len)
+        _pad_left(list_2, 0, max_len)
 
 
 def _make_whole_list_negative(a_list):
@@ -70,11 +66,11 @@ def _check_if_negative(a_list):
     return a_list
 
 
-def _formate_numbers(num_list_1, num_list_2):
+def _format_numbers(num_list_1, num_list_2):
     num_list_1 = _check_if_negative(num_list_1)
     num_list_2 = _check_if_negative(num_list_2)
     
-    _make_lists_equal_length(num_list_1, num_list_2, False)
+    _make_lists_equal_length(num_list_1, num_list_2)
 
 
 def add(num_list_1, num_list_2, base_list):
@@ -82,9 +78,10 @@ def add(num_list_1, num_list_2, base_list):
     num_list_1 + num_list_2
 
     base_list is a list of the bases of the 2 numbers this function is supposed to work with
+    It must be greater than or equal to the length of the two number lists
     """
     
-    _formate_numbers(num_list_1, num_list_2)
+    _format_numbers(num_list_1, num_list_2)
     
     return clean_up_bases([x + y for x, y in zip(num_list_1, num_list_2)], base_list)
 
@@ -94,18 +91,19 @@ def subtract(num_list_1, num_list_2, base_list):
     num_list_1 - num_list_2
 
     base_list is a list of the bases of the 2 numbers this function is supposed to work with
+    It must be greater than or equal to the length of the two number lists
     """
 
-    _formate_numbers(num_list_1, num_list_2)
+    _format_numbers(num_list_1, num_list_2)
 
-    negative_flag = _compare_numbers_represented_as_arrays(num_list_1, num_list_2)
+    comparison = _compare_numbers_represented_as_arrays(num_list_1, num_list_2)
 
-    if negative_flag == -1:
+    if comparison == -1:
         num_list_1, num_list_2 = num_list_2, num_list_1
 
     result = clean_up_bases([x - y for x, y in zip(num_list_1, num_list_2)], base_list)
 
-    if negative_flag == -1 and result[0] > 0:
+    if comparison == -1 and result[0] > 0:
         result[0] *= -1
 
     return result
@@ -113,11 +111,13 @@ def subtract(num_list_1, num_list_2, base_list):
 
 def clean_up_bases(a_list, base_list):
     """
-    this "cleans up" the result of the addition or subtraction to give the right asnwer to the operation
+    This "cleans up" the result of the addition or subtraction to give the right asnwer to the operation
 
-    since there is a chance the list of bases is not long enough to include a base for the carry,
+    Since there is a chance the list of bases is not long enough to include a base for the carry,
     like -99 - 1 = -100 where the list of bases may be [10, 10],
     this function tries to correct the carry by extending the list of bases using the most significant base
+    
+    the list of bases must be greater than or equal to the length of the list of values
     """
     a_list = clean_up_bases_ignore_most_significant_digit(a_list, base_list)
 
@@ -125,10 +125,10 @@ def clean_up_bases(a_list, base_list):
     # this deals with the most significant digit if the positive or negative value of it is greater than or euqal to the base
     
     if len(a_list) > len(base_list):
-        while a_list[0] >= base_list[0] or -a_list[0] >= base_list[0]:
+        while abs(a_list[0]) >= base_list[0]:
             carry = 0
 
-            if -a_list[0] >= base_list[0]:
+            if a_list[0] < 0:
                 a_list[0] *= -1
                 carry = -(a_list[0] // base_list[0])
             else:
@@ -142,11 +142,13 @@ def clean_up_bases(a_list, base_list):
 
 def clean_up_bases_ignore_most_significant_digit(a_list, base_list):
     """
-    this "cleans up" the result of the addition or subtraction to give the right asnwer to the operation
+    This "cleans up" the result of the addition or subtraction to give the right asnwer to the operation
 
-    since there is a chance the list of bases is not long enough to include a base for the carry,
+    Since there is a chance the list of bases is not long enough to include a base for the carry,
     like -99 - 1 = -100 where the list of bases may be [10, 10],
     this function will not try to correct the carry or the -1 in the example
+
+    The list of bases must be greater than or equal to the length of the list of values
     """
 
     # remove leading zeros
@@ -166,6 +168,7 @@ def clean_up_bases_ignore_most_significant_digit(a_list, base_list):
     for x, (value, base) in enumerate(zip(a_list, base_list)):
         if value >= base or value < 0:
             carry = 0
+
             if -value >= base:
                 value *= -1
                 carry = -(value // base)
@@ -177,7 +180,9 @@ def clean_up_bases_ignore_most_significant_digit(a_list, base_list):
             else:
                 if value < base:
                     break
+                
                 a_list.append(carry)
+            
             a_list[x] = value % base
 
     a_list.reverse()
@@ -188,3 +193,8 @@ def clean_up_bases_ignore_most_significant_digit(a_list, base_list):
         a_list = _remove_leading_element(a_list)
 
     return a_list
+
+
+if __name__ == "__main__":
+    # here is an example
+    print(add([1, 20], [2, 41], [24, 60]))
