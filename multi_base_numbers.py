0b358ba31ccbed408e569820eee1a178684a771d
TelevisionNinja <TelevisionNinja@gmail.com>
2020-07-27 20:26:15 -0500

    renamed variables

--- multi_base_numbers.py
+++ multi_base_numbers.py
@@ -1,33 +1,33 @@
-def _pad_left(a_list, length):
+def _pad_left(num, length):
     """
     pad zeros to the left of a list
     """
 
-    return [0 for _ in range(length - len(a_list))] + a_list
+    return [0 for _ in range(length - len(num))] + num
 
 
-def _remove_leading_element(a_list):
-    for x, value in enumerate(a_list):
+def _remove_leading_zeros(num):
+    for x, value in enumerate(num):
         if value != 0:
-            return a_list[x:]
+            return num[x:]
 
     return [0]
 
 
 def _compare_nums(num_lists):
     """
-    compares two numbers that are represented as lists
+    compares numbers that are represented as lists
 
     both lists must be the same length
 
     1 : list 1 is greater
     0 : both are equal
-    -1 : list 2 is greater
+    -1 : list 2-n is greater
     """
 
-    for l in zip(*num_lists):
-        val_1 = l[0]
-        val_2 = sum(l[1:])
+    for values in zip(*num_lists):
+        val_1 = values[0]
+        val_2 = sum(values[1:])
 
         if val_2 > val_1:
             return -1
@@ -41,30 +41,30 @@ def _format_list(num_lists):
     nums = []
     max_len = 0
 
-    for l in num_lists:
-        l = _format_num(l)
-        nums.append(l)
-        length = len(l)
+    for a_num in num_lists:
+        a_num = _format_num(a_num)
+        nums.append(a_num)
+        length = len(a_num)
         if length > max_len:
             max_len = length
 
-    for x, l in enumerate(nums):
-        nums[x] = _pad_left(l, max_len)
+    for x, a_num in enumerate(nums):
+        nums[x] = _pad_left(a_num, max_len)
 
     return nums
 
 
-def _format_num(a_list):
+def _format_num(num):
     # remove leading zeros
-    if a_list[0] == 0:
-        a_list = _remove_leading_element(a_list)
+    if num[0] == 0:
+        num = _remove_leading_zeros(num)
 
     # check if the number is negative
-    if a_list[0] < 0:
+    if num[0] < 0:
         # make the whole list negative
-        return [-x if x > 0 else x for x in a_list]
+        return [-x if x > 0 else x for x in num]
 
-    return a_list
+    return num
 
 
 def add(base_list, *num_lists):
@@ -92,16 +92,16 @@ def subtract(base_list, *num_lists):
     # compare the 1st number against the rest
     comparison = _compare_nums(nums)
 
-    values = []
+    result = []
 
     if comparison == 1:
-        values = [l[0] - sum(l[1:]) for l in zip(*nums)]
+        result = [values[0] - sum(values[1:]) for values in zip(*nums)]
     elif comparison == -1:
-        values = [sum(l[1:]) - l[0] for l in zip(*nums)]
+        result = [sum(values[1:]) - values[0] for values in zip(*nums)]
     else:
         return [0]
 
-    result = clean_up_bases(base_list, values)
+    result = clean_up_bases(base_list, result)
 
     if comparison == -1:
         result[0] *= -1
@@ -109,7 +109,7 @@ def subtract(base_list, *num_lists):
     return result
 
 
-def clean_up_bases(base_list, a_list):
+def clean_up_bases(base_list, value_list):
     """
     This "cleans up" the result of the addition or subtraction to give the right answer to the operation
 
@@ -122,11 +122,11 @@ def clean_up_bases(base_list, a_list):
     The list of values must not have leading zeros
     """
 
-    a_list = clean_up_bases_ignore_most_significant_digit(base_list, a_list)
+    value_list = clean_up_bases_ignore_most_significant_digit(base_list, value_list)
 
-    if len(a_list) > len(base_list):
+    if len(value_list) > len(base_list):
         base = base_list[0]
-        value = a_list[0]
+        value = value_list[0]
 
         while abs(value) >= base:
             carry = value // base
@@ -135,15 +135,15 @@ def clean_up_bases(base_list, a_list):
                 value *= -1
                 carry = -(value // base)
 
-            a_list.insert(0, carry)
-            a_list[1] = value % base
+            value_list.insert(0, carry)
+            value_list[1] = value % base
 
             value = carry
 
-    return a_list
+    return value_list
 
 
-def clean_up_bases_ignore_most_significant_digit(base_list, a_list):
+def clean_up_bases_ignore_most_significant_digit(base_list, value_list):
     """
     This "cleans up" the result of the addition or subtraction to give the right answer to the operation
 
@@ -159,45 +159,45 @@ def clean_up_bases_ignore_most_significant_digit(base_list, a_list):
     # check if it's a negative number and correctly format it
     negative_flag = True
 
-    for x in a_list:
+    for x in value_list:
         if x > 0:
             negative_flag = False
             break
     else:
-        a_list = [-x for x in a_list]
+        value_list = [-x for x in value_list]
 
-    a_list.reverse()
+    value_list.reverse()
     base_list.reverse()
 
-    length = len(a_list) - 1
+    length = len(value_list) - 1
 
-    for x, (value, base) in enumerate(zip(a_list, base_list)):
+    for x, (value, base) in enumerate(zip(value_list, base_list)):
         if value >= base or value < 0:
-            carry = value // base # floor division is necessary
+            carry = value // base  # floor division is necessary
 
             if -value >= base:
                 value *= -1
                 carry = -(value // base)
 
-            a_list[x] = value % base
+            value_list[x] = value % base
 
             if x < length:
-                a_list[x + 1] += carry
+                value_list[x + 1] += carry
             else:
-                a_list += [carry]
+                value_list += [carry]
                 length += 1
 
-    a_list.reverse()
+    value_list.reverse()
     base_list.reverse()
 
     # removing leading zeros
-    if a_list[0] == 0:
-        a_list = _remove_leading_element(a_list)
+    if value_list[0] == 0:
+        value_list = _remove_leading_zeros(value_list)
 
     if negative_flag:
-        a_list[0] *= -1
+        value_list[0] *= -1
 
-    return a_list
+    return value_list
 
 
 if __name__ == "__main__":
